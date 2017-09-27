d2cde5d376cc76a19cd099aead3b977777f1dc71
Ben Arena <barena@managedsolution.com>
2017-09-26 18:29:50 -0700

    completed login button

--- src/main/java/com/example/springbootoauth2/SpringBootOauth2Application.java
+++ src/main/java/com/example/springbootoauth2/SpringBootOauth2Application.java
@@ -3,12 +3,35 @@ package com.example.springbootoauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class SpringBootOauth2Application {
+@RestController
+public class SpringBootOauth2Application extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootOauth2Application.class, args);
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+                .antMatcher("/**")
+				.authorizeRequests()
+				    .antMatchers("/", "/login**", "/webjars/**")
+				    .permitAll()
+				.anyRequest()
+				    .authenticated();
 	}
 }
